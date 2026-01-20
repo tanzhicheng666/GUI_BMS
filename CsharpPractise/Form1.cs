f3fdc004ad5d2d0bdf44e4aa2ff7acb7fbb78c41
Zhicheng Tan <zhicheng.tan@qorvo.com>
2026-01-20 15:06:23 +0800

    add a WPF project to solution

--- CsharpPractise/Form1.cs
+++ CsharpPractise/Form1.cs
@@ -14,7 +14,7 @@ namespace CsharpPractise
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent(); 
         }
     }
 }
--- basic_knowledge/Program.cs
+++ basic_knowledge/Program.cs
@@ -11,7 +11,17 @@ namespace basic_knowledge
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
-            Console.ReadLine();
+            Console.WriteLine($"byte: {sizeof(byte)}  byte");
+            Console.WriteLine($"byte: {sizeof(sbyte)}  byte");
+            Console.WriteLine($"short: {sizeof(short)}  byte");
+            Console.WriteLine($"ushort: {sizeof(ushort)}  byte");
+            Console.WriteLine($"int: {sizeof(int)}  byte");
+            Console.WriteLine($"long: {sizeof(long)}  byte");
+            Console.WriteLine($"float: {sizeof(float)}  byte");
+            Console.WriteLine($"double: {sizeof(double)}  byte");
+            //Console.ReadLine();
+            Console.ReadKey();
+            
         }
     }
 }
